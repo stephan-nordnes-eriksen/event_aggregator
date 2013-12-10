0fe619e5f6714ad6c72aa4dd88f2076ddf8b9c3d
Stephan Eriksen <stephan.n.eriksen@gmail.com>
2013-12-10 19:33:28 +0100

    adding skeleton spec files for all classes

---
+++ spec/event_aggregator/listener_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Listener do
+    describe "Register Listener" do
+        it "should increase the listener count for 'foo'" do
+            
+        end
+    end
+end
