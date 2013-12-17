99e28d5a2bc97f925e3151fd75218fc37ddcc888
Stephan Eriksen <stephan.n.eriksen@gmail.com>
2013-12-17 23:19:25 +0100

    adding pending statements to all specs

--- spec/lib/event_aggregator/listener_spec.rb
+++ spec/lib/event_aggregator/listener_spec.rb
@@ -2,20 +2,26 @@ require 'spec_helper'
 
 describe EventAggregator::Listener do
 	describe '.recieve_message' do
-		
+		it 'pending' do
+			pending "not implemented"
+		end
 	end
 	
 	describe '.message_type_to_recieve_add' do
-		
+		it 'pending' do
+			pending "not implemented"
+		end
 	end
 
 	describe '.message_type_to_recieve_remove' do
-		
+		it 'pending' do
+			pending "not implemented"
+		end
 	end
 
     describe "Register Listener" do
         it "should increase the listener count for 'foo'" do
-            
+           pending "not implemented" 
         end
     end
 end
--- spec/lib/event_aggregator/message_spec.rb
+++ spec/lib/event_aggregator/message_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe EventAggregator::Message do
     describe '.publish' do
-		
+		it 'pending' do
+			pending "not implemented"
+		end
 	end
 end
