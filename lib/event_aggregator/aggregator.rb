c7b9db3d617f8f73a097ad7808d59971418cfb63
Stephan Eriksen <stephan.n.eriksen@gmail.com>
2013-12-20 03:24:58 +0100

    Updating aggregator.rb documentation and removing some comments.

--- lib/event_aggregator/aggregator.rb
+++ lib/event_aggregator/aggregator.rb
@@ -1,5 +1,3 @@
-#require 'singleton'
-
 module EventAggregator
 
 	# Public: TODO: Could potentially turn this into a module.
@@ -12,24 +10,50 @@ module EventAggregator
 	# 		end
 	# 	end
 	class Aggregator
-		#include Singleton
 		class <<self; private :new; end
-		#TODO: Figure out how to do singleton pattern properly
 
 		@@listeners = Hash.new{|h, k| h[k] = []}
-
+		
+		# Public: Register an EventAggregator::Listener to recieve
+		# 		  a specified message type
+		#
+		# listener - An EventAggregator::Listener which should recieve 
+		# 			 the messages.
+		# message_type -The message type to recieve. Can be anything except nil.
+		# 				 Often it is preferable to use a string eg. "Message Type".
+		#
+		# Returns True if listener is added. #TODO: Verify this
 		def self.register( listener, message_type )
 			@@listeners[message_type] << listener unless ! (listener.class < EventAggregator::Listener) || @@listeners[message_type].include?(listener)
 		end
-
+		
+		# Public: Unegister an EventAggregator::Listener to a 
+		# 		  specified message type. The listener will no
+		# 		  longer get messages of this type.
+		# 
+		# listener - The EventAggregator::Listener which should no longer recieve 
+		# 			 the messages.
+		# message_type - The message type to unregister for.
+		#
+		# Returns True if listener is no longer recieving this message type. #TODO: Verify this
 		def self.unregister( listener, message_type )
 			@@listeners[message_type].delete listener
 		end
+		
+		# Public: As Unregister, but will unregister listener from all message types.
+		#
+		# listener -The listener who should no longer get any messages at all, 
+		# 			 regardless of type.
 		def self.unregister_all( listener )
 			@@listeners.each do |e|
 				e[1].delete(listener)
 			end
 		end
+		
+		# Public: Will publish the specified message to all listeners
+		# 		  who has registered for this message type.
+		#
+		# message -The message to be distributed to the listeners.
 		def self.message_publish ( message )
 			raise "Invalid message" unless message.is_a? EventAggregator::Message
 
